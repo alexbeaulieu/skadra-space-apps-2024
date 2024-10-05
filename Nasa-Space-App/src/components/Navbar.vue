c426e227ab729748047ab4e7292ed6f15a4c59d2
elanglois <elanglois@lti.ca>
2024-10-05 12:54:17 -0400

    Front-end updated nav

---
+++ Nasa-Space-App/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import router from '@/router';
+
+</script>
+
+<template>
+  <div class="navbar navbar-expand-lg navbar-dark sticky-top bg-primary">
+    <div class="container">
+      <div class="align-items-center navbar-nav">
+        <RouterLink class="navbar-brand" :to="{ name: 'home' }">
+          <img             
+            class="d-inline-block rounded-circle"
+            src="../assets/nasalogo.png"
+            alt="CPCDQ"
+            width="60">
+          Accueil
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
